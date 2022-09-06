928f155c943565dfd032e34e268cd191fb902dbc
Nicolas Gonzalez <nicoaugustogonzalez14@gmail.com>
2022-09-06 19:46:53 -0300

    load notes from Firestone

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,12 +1,14 @@
+import { async } from "@firebase/util";
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./";
+import { loadNotes } from "../../helpers/loadNotes";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./";
 
 export const startNewNote = () => {
     return async( dispatch, getState ) => {
         
         dispatch( savingNewNote() );
-        
+
         const { uid } = getState().auth;
 
         const newNote = {
@@ -22,6 +24,20 @@ export const startNewNote = () => {
 
         dispatch( addNewEmptyNote( newNote ) );
         dispatch( setActiveNote( newNote ) );
+
+    }
+}
+
+export const startLoadingNotes = () => {
+    return async( dispatch , getState ) => {
+
+        const { uid } = getState().auth;
+        if ( !uid ) throw new Error("UID of user doesn't exist");
+        console.log(uid);
+
+        const notes = await loadNotes( uid );
+        
+        dispatch( setNotes( notes ) );
 
     }
 }
