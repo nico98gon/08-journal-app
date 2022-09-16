7482980ad268c9e8e09323012b14e8a94b80f54d
Nicolas Gonzalez <nicoaugustogonzalez14@gmail.com>
2022-09-15 23:05:24 -0300

    321. & 322. Environments variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,23 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite'
+import { getEnvironments } from "../helpers/getEnvironments";
+
+const {
+    VITE_APIKEY,
+    VITE_AUTHDOMAIN,
+    VITE_PROJECTID,
+    VITE_STORAGEBUCKET,
+    VITE_MESSAGINGSENDERID,
+    VITE_APPID,
+    VITE_MEASUREMENTID
+} = getEnvironments();
+
+// const env = getEnvironments();
+
+// console.log( env );
+// console.log( process.env );
+// console.log( import.meta.env );
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,15 +35,25 @@ import { getFirestore } from 'firebase/firestore/lite'
 // };
 
 // Testing
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAe0Lt5IIbcFqejHR4R-iuDA2SfgzzAPWU",
+//     authDomain: "journal-app-testing-7e1e4.firebaseapp.com",
+//     projectId: "journal-app-testing-7e1e4",
+//     storageBucket: "journal-app-testing-7e1e4.appspot.com",
+//     messagingSenderId: "532594777337",
+//     appId: "1:532594777337:web:145d40d587a1277b149628",
+//     measurementId: "G-P78BY8N5SG"
+//     };
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAe0Lt5IIbcFqejHR4R-iuDA2SfgzzAPWU",
-    authDomain: "journal-app-testing-7e1e4.firebaseapp.com",
-    projectId: "journal-app-testing-7e1e4",
-    storageBucket: "journal-app-testing-7e1e4.appspot.com",
-    messagingSenderId: "532594777337",
-    appId: "1:532594777337:web:145d40d587a1277b149628",
-    measurementId: "G-P78BY8N5SG"
-    };
+    apiKey: VITE_APIKEY,
+    authDomain: VITE_AUTHDOMAIN,
+    projectId: VITE_PROJECTID,
+    storageBucket: VITE_STORAGEBUCKET,
+    messagingSenderId: VITE_MESSAGINGSENDERID,
+    appId: VITE_APPID,
+    measurementId: VITE_MEASUREMENTID
+};
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp( firebaseConfig );
--- tests/store/journal/thunks.test.js
+++ tests/store/journal/thunks.test.js
@@ -1,4 +1,6 @@
-import { startNewNote } from "../../../src/store/journal";
+import { collection, deleteDoc, getDocs } from "firebase/firestore/lite";
+import { FirebaseDB } from "../../../src/firebase/config";
+import { addNewEmptyNote, savingNewNote, setActiveNote, startNewNote } from "../../../src/store/journal";
 
 describe('testing journalThunks', () => {
 
@@ -13,5 +15,29 @@ describe('testing journalThunks', () => {
         getState.mockReturnValue({ auth: { uid: uid } });
 
         await startNewNote()( dispatch, getState );
+
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+
+        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+            body: '',
+            title: '',
+            id: expect.any( String ),
+            date: expect.any( Number )
+        }));
+
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title: '',
+            id: expect.any( String ),
+            date: expect.any( Number )
+        }));
+
+        // Delete from firebase
+        const collectionRef = collection( FirebaseDB, `${ uid }/journal/notes`);
+        const docs = await getDocs( collectionRef );
+
+        const deletePromises = [];
+        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
+        await Promise.all( deletePromises );
     });
 });
